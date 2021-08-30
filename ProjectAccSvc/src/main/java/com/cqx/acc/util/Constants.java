b9491607e87cbfd2c63766b95040454a885a9061
chenqixu <13509323824@139.com>
2021-08-30 11:30:12 +0800

    配置加载有点乱，先整理了一版

--- ProjectAccSvc/src/main/java/com/cqx/acc/util/Constants.java
+++ ProjectAccSvc/src/main/java/com/cqx/acc/util/Constants.java
@@ -1,70 +1,69 @@
 package com.cqx.acc.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Properties;
-
+import com.cqx.acc.properites.bean.Db;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
-import com.cqx.acc.properites.bean.Db;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Constants {
-	public static final boolean ISLOGGER = false;
-	public static final boolean ISINFO = false;
-	public static final boolean ISDEBUG = false;
-	public static final boolean ISERR = true;
-	public static final boolean IS_PERMIT = false; // 是否鉴权，开发时候用false
-	public static final String DECODE = "UTF-8";
-	
-	public static String spltstr = File.separator;//文件路径分隔符(区分windows和linux)
+    public static final boolean ISLOGGER = true;
+    public static final boolean ISINFO = true;
+    public static final boolean ISDEBUG = false;
+    public static final boolean ISERR = true;
+    public static final boolean IS_PERMIT = false; // 是否鉴权，开发时候用false
+    public static final String DECODE = "UTF-8";
 
-	public static String configPath = getConfigPath();
-	
-	private static Resource dbResource = new ClassPathResource(spltstr+
-			"resources"+spltstr+"db.properties");
-	private static Properties props = null;
-		
-	public static String getConfigPath(){
-		return Constants.class.getClassLoader().getResource("").getPath();
-	};
-	
-	@Autowired
-	private Db db;	
-	public static String dbDriver = "";	
-	public static String dbUserName = "";	
-	public static String dbPassword = "";	
-	public static String dbURL = "";
+    public static String spltstr = File.separator;//文件路径分隔符(区分windows和linux)
 
-	public Db getDb() {
-		return db;
-	}
-	
-	/**
-	 * 通过id获得配置文件common.properties的值
-	 * */
-	public static String getCommonPropertiesValueById(String id){
-		String value = "";
-		try{
-			value = props.getProperty(id);
-		}catch(Exception e){
-			CommonUtils.error(e.getMessage());
-		}
-		return value;
-	}
-	
-	public static void init(){
-		//日志初始化
-		PropertyConfigurator.configure(configPath+spltstr+"resources"
-				+spltstr+"config"+spltstr+"log4j.properties");
-		try {
-			//配置初始化
-			props  = PropertiesLoaderUtils.loadProperties(dbResource);
-		} catch (IOException e) {
-			CommonUtils.error(e.toString());
-		}
-	}
+    public static String configPath = getConfigPath();
+
+    public static String finalConfigPath = spltstr + "resources" + spltstr + "config" + spltstr;
+
+    private static Resource dbResource = new ClassPathResource(finalConfigPath + "db.properties");
+    private static Properties props = null;
+
+    public static String getConfigPath() {
+        return Constants.class.getClassLoader().getResource("").getPath();
+    }
+
+    @Autowired
+    private Db db;
+    public static String dbDriver = "";
+    public static String dbUserName = "";
+    public static String dbPassword = "";
+    public static String dbURL = "";
+
+    public Db getDb() {
+        return db;
+    }
+
+    /**
+     * 通过id获得配置文件common.properties的值
+     */
+    public static String getCommonPropertiesValueById(String id) {
+        String value = "";
+        try {
+            value = props.getProperty(id);
+        } catch (Exception e) {
+            CommonUtils.error(e.getMessage());
+        }
+        return value;
+    }
+
+    public static void init() {
+        //日志初始化
+        PropertyConfigurator.configure(configPath + finalConfigPath + "log4j.properties");
+        try {
+            //配置初始化
+            props = PropertiesLoaderUtils.loadProperties(dbResource);
+        } catch (IOException e) {
+            CommonUtils.error(e.toString());
+        }
+    }
 }
